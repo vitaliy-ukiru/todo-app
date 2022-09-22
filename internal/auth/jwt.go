9d412e421cd48b53f7b9d89dae29771941f5ea1f
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2022-09-22 19:27:16 +0500

    upgrade auth.JwtService, add method for login

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"context"
 	"time"
 
 	"github.com/google/uuid"
@@ -16,13 +17,48 @@ type Claims struct {
 	jwt.RegisteredClaims
 }
 
+type UserDTO struct {
+	ID        uuid.UUID `json:"id,omitempty"`
+	CreatedAt time.Time `json:"created_at,omitempty"`
+	Email     string    `json:"email"`
+	Username  string    `json:"username"`
+}
+
+type UserChecker interface {
+	CheckCredentials(ctx context.Context, email string, password string) (*user.User, error)
+}
+
 type JwtService struct {
 	provider        *jwt.Provider
 	accessTokenLife time.Duration
+	userUC          UserChecker
 }
 
 func NewJwtService(provider *jwt.Provider, accessTokenLife time.Duration) *JwtService {
 	return &JwtService{provider: provider, accessTokenLife: accessTokenLife}
+}
+
+type CredentialsDTO struct {
+	Email    string
+	Password string
+}
+
+type LoginResult struct {
+	Token     string    `json:"access_token"`
+	ExpiresAt time.Time `json:"expires_at"`
+}
+
+func (j JwtService) Login(ctx context.Context, cred CredentialsDTO) (LoginResult, error) {
+	u, err := j.userUC.CheckCredentials(ctx, cred.Email, cred.Password)
+	if err != nil {
+		return LoginResult{}, errors.WithStack(err)
+	}
+	token, expiresAt, err := j.NewAccessToken(*u)
+	if err != nil {
+		return LoginResult{}, errors.WithStack(err)
+	}
+
+	return LoginResult{Token: token, ExpiresAt: expiresAt}, nil
 }
 
 func (j JwtService) NewAccessToken(u user.User) (string, time.Time, error) {
